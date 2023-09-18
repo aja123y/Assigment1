38fdb4ba8740112af231834a1fad674bf0ff4426
aja123y <145337871+aja123y@users.noreply.github.com>
2023-09-18 15:09:32 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,101 @@
+// let a = 5;
+// let b = 5;
+// let c = a-b;
+// console.log(c);
+
+// let a = 6;
+// let b = 5;
+// let c = a + b;
+// console.log(c);
+
+// let a = 5;
+// let b = 6;
+// let c = a*b;
+// console.log(c);
+
+// let a = 6;
+// let b = 6;
+// let c = a/b;
+// console.log(c);
+
+// == , === , < , >
+
+// let a = 4;
+// let b = 6;
+// console.log(a<=b);
+
+// let a = 5;
+// let b = 6;
+// console.log(a<=b);
+
+// let a = 4;
+// let b = 5;
+// console.log(a>b);
+
+// a = 43;
+// console.log(a);
+// var a;
+
+// const  b = 53;
+// console.log(b);
+
+// let a = 5;
+// let b = 5;
+// console.log(a!==b)
+
+// let a = 20;
+// a += 5;
+// console.log(a);
+
+// let a = 20;
+// a -= 5;
+// console.log(a);
+
+// let a = 20;
+// a *= 4;
+// console.log(a);]
+
+// let a = 20;
+// a /= 5;
+// console.log(a);
+
+// let a = 20;
+// a %= 5;
+// console.log(a);
+
+// let a = 20;
+// console.log(a,a++);
+// console.log(++a,a);
+
+// let b = 10;
+// console.log(b++,b);
+
+// let a = 4;
+// let b = 2;
+// let c = 6;
+// console.log(a>b && b<c);
+// console.log(a>b || b<c);
+// console.log(!a>b);
+
+// let x = 5;
+// if(x > 10)
+// {
+//     console.lof('x is greater than 10');
+// }
+// else
+// {
+//     console.log('X is less than or equal 10');
+// }
+
+// let a = 19;
+// if(a>=18)
+// {
+//     console.log('Your are able to Drive...');
+// }
+// else
+// {
+//     console.log('Your are not able to Drive...');
+// }
+
+
+
